9507efc8b5996b1dd11c68eb98f255b46503895d
Abhiram Gunisetty <abhi.gunisetti@gmail.com>
2021-07-15 19:49:34 +0530

    game over screen is added and other changes have been introduced

--- App.js
+++ App.js
@@ -5,19 +5,33 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/header';
 import StartGameScreen from './screens/startGameScreen';
 import GameScreen from './screens/gameScreen';
+import GameOverScreen from './screens/gameOverScreen';
 
 export default function App() {
 
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const newGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
   };
 
+  const gameOverhandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
   let content = <StartGameScreen onStartGame={startGameHandler} />
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverhandler} />
+  }
+  else if (guessRounds > 0) {
+    content = <GameOverScreen userNumber={userNumber} roundNumber={guessRounds} onRestart={newGameHandler} />
   }
   return (
     <View style={styles.screen} >
