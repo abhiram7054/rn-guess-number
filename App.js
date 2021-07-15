2802256bac0fef462831ed9ceb0e465fb182a0e7
Abhiram Gunisetty <abhi.gunisetti@gmail.com>
2021-07-15 11:24:50 +0530

    always keep the function name starts with Uppercase

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/header';
+import StartGameScreen from './screens/startGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen} >
       <Header title="Guess the Number" />
+      <StartGameScreen />
     </View>
   );
 }
