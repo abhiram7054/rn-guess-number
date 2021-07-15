61cdae4d161cb61a667e4e3e2601b630c2d77362
Abhiram Gunisetty <abhi.gunisetti@gmail.com>
2021-07-15 14:52:00 +0530

    made various changes to start Game screen related to inputs it take and keyboard activities. do check it out

--- screens/startGameScreen.js
+++ screens/startGameScreen.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard,
+ } from "react-native";
 
 import Card from "../components/card";
 import Colors from "../constants/colors";
 import Input from "../components/input";
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return(
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss();}}> 
         <View style={styles.screen} >
             <Text style={styles.title}>Start a New Game!</Text> 
             <Card style={styles.inputContainer}>
                 <View style={styles.inputContainer}>
                     <Text> Enter a Number </Text>
-                    <Input style={styles.input} blurOnSubmit autoCapitalize="none" autoCorrect={false} keyboardType="number-pad" maxLength ={2} />
+                    <Input style={styles.input} 
+                    blurOnSubmit 
+                    autoCapitalize="none" 
+                    autoCorrect={false} 
+                    keyboardType="number-pad" 
+                    maxLength ={2}
+                    onChangeText = {numberInputHandler}
+                    value = {enteredValue} />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}><Button color={Colors.secondary} title="RESET" onPress={() => {}} /></View>
                         <View style={styles.button}><Button color={Colors.primary} title="CONFIRM" onPress={() => {}} /></View>
@@ -20,6 +43,7 @@ const StartGameScreen = props => {
                 </View> 
             </Card>
         </View> 
+        </TouchableWithoutFeedback>
     );
 };
 
