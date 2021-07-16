832d7d05884f8b915db6b742ca3cf1dfdbca1a7e
Abhiram Gunisetty <abhi.gunisetti@gmail.com>
2021-07-16 10:25:49 +0530

    adding images and fonts

--- screens/gameOverScreen.js
+++ screens/gameOverScreen.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import BodyText from "../components/bodyText";
+import BoldText from "../components/boldText";
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text style={styles.fontBold}>The Game is Over!!</Text>
-            <Text style={styles.fontRegular}>The number of rounds are: {props.roundNumber}</Text>
-            <Text style={styles.fontRegular}>The number was: {props.userNumber}</Text>
+            <BoldText>The Game is Over!!</BoldText>
+            <View style={styles.imageContainer} >
+                <Image source={require("../assets/gameover.png")} style={styles.image} resizeMode="cover" />
+            </View>
+            <BodyText>The number of rounds are: {props.roundNumber}</BodyText>
+            <BodyText>The number was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestart} />
         </View> 
     );
@@ -18,13 +23,17 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         alignItems: "center",
     },
-    fontRegular: {
-        fontFamily: "regular",
+    imageContainer: {
+        width: "80%",
+        height: 300,
+        borderRadius: 100,
+        overflow: "hidden",
+        marginVertical: 30,
     },
-    fontBold: {
-        fontFamily: "bold",
+    image: {
+        width: "100%",
+        height: "100%"
     }
-
 });
 
 export default GameOverScreen;
